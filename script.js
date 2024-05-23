d71100373e5274e02be2f671e98eb26132c3abc2
Feyzullah Durası <128799284+feyzullahdurasi@users.noreply.github.com>
2024-05-23 22:34:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ function calculateTotal() {
     count.innerText = selectedSeatCount;
     amount.innerText = selectedSeatCount * select.value;
 }
+
+const ads1 = [
+    '/Users/feyzullahdurasi/Downloads/Sinema-Koltuk-Rezervasyon-main/ads/AK1.png',
+    '/Users/feyzullahdurasi/Downloads/Sinema-Koltuk-Rezervasyon-main/ads/AK2.png'
+];
+
+
+const ads2 = [
+    '/Users/feyzullahdurasi/Downloads/Sinema-Koltuk-Rezervasyon-main/ads/MCD1.png',
+    '/Users/feyzullahdurasi/Downloads/Sinema-Koltuk-Rezervasyon-main/ads/MCD2.png'
+];
+
+let currentIndex = 0;
+
+
+function changeImage() {
+    const imgElement = document.getElementById('sagAds');
+    const imgElement2 = document.getElementById('solAds');
+    currentIndex = (currentIndex + 1) % ads1.length;
+    currentIndex = (currentIndex + 1) % ads2.length;
+    imgElement.src = ads1[currentIndex];
+    imgElement2.src = ads2[currentIndex];
+}
+
+setInterval(changeImage, 5000);
